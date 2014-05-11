bfe858969d14fc425efc247b7c11ec10dbdc64bd
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2014-05-11 21:50:19 +0200

    Display time since last commit

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,15 +1,4 @@
 function fish_prompt
-	
-  if not set -q -g __fish_robbyrussell_functions_defined
-    set -g __fish_robbyrussell_functions_defined
-    function _git_branch_name
-      echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-    end
-	
-    function _is_git_dirty
-      echo (git status -s --ignore-submodules=dirty ^/dev/null)
-    end
-  end
 
   set -l cyan (set_color -o cyan)
   set -l yellow (set_color -o yellow)
@@ -22,9 +11,10 @@ function fish_prompt
 
   if [ (_git_branch_name) ]
     set -l git_branch $red(_git_branch_name)
-    set git_info "$blue git:($git_branch$blue)"
+    set -l git_commit_time (git_time_since_commit)
+    set git_info "$blue git:($git_commit_time|$git_branch$blue)"
 
-    if [ (_is_git_dirty) ]
+    if _git_is_dirty
       set -l dirty "$yellow ✗"
       set git_info "$git_info$dirty"
     end
