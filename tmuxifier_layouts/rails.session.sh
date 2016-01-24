620b6e8898a5f217ae969afd97abbf19337543ae
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2016-01-24 18:52:56 +0100

    Clean up tmuxifier config
    - Add `workon <path>` to quickly launch a new tmux session with a default tmux
      layout
    - Remove unused tmuxifier layouts

--- tmuxifier_layouts/rails.session.sh
+++ tmuxifier_layouts/rails.session.sh
@@ -1,10 +1,16 @@
+if [[ -z "$TMUXIFIER_SESSION_DIR" ]]; then
+  echo "You have to specify TMUXIFIER_SESSION_DIR!"
+  exit 1
+fi
+
 # Set a custom session root path. Default is `$HOME`.
 # Must be called before `initialize_session`.
-session_root "~/dev/digital_natives/datahub/admin/"
+session_root $TMUXIFIER_SESSION_DIR
+SESSION_NAME=$(basename $TMUXIFIER_SESSION_DIR)
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
-if initialize_session 'dh_admin'; then
+if initialize_session $SESSION_NAME; then
 
   # Create a new window inline within session layout definition.
   new_window "vim"
@@ -12,12 +18,11 @@ if initialize_session 'dh_admin'; then
   new_window "console"
   new_window "tmp"
   new_window 'log'
-    run_cmd "tail -f log/development.log"
+    run_cmd "tail -f log/*.log"
   new_window 'server'
 
   # Select the default active window on session creation.
   select_window 0
-
 fi
 
 # Finalize session creation and switch/attach to it.
