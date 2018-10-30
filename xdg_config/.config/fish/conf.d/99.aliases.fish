ea014e6dd5980e3e8e571e5cafe4256e487f4fa7
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2018-10-30 08:44:41 +0000

    Fix aliases with $EDITOR

--- xdg_config/.config/fish/conf.d/99.aliases.fish
+++ xdg_config/.config/fish/conf.d/99.aliases.fish
@@ -1,9 +1,9 @@
 # Quick edits
-function ef; $EDITOR ~/.config/fish/conf.d; end
-function ev; $EDITOR ~/.config/nvim/init.vim; end
-function eg; $EDITOR ~/.gitconfig; end
-function et; $EDITOR ~/.tmux.conf; end
-function es; $EDITOR ~/.ssh/config; end
+function ef; eval $EDITOR ~/.config/fish/conf.d; end
+function ev; eval $EDITOR ~/.config/nvim/init.vim; end
+function eg; eval $EDITOR ~/.gitconfig; end
+function et; eval $EDITOR ~/.tmux.conf; end
+function es; eval $EDITOR ~/.ssh/config; end
 function eh; sudo $EDITOR /etc/hosts; end
 
 # Shortcuts
