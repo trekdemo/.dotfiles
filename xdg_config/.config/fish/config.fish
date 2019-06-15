06e4470966cc084c6fd94ff07bf0ad7b578c759f
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2019-06-15 21:36:10 +0200

    Uninstall oh-my-fish

--- xdg_config/.config/fish/config.fish
+++ xdg_config/.config/fish/config.fish
@@ -1,17 +1,3 @@
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
-
-# virtual env wrappers for Python
-# eval (python -m virtualfish)
-if type --quiet rbenv
-  status --is-interactive; and source (rbenv init -|psub)
-end
-
 # BEGIN ANSIBLE MANAGED BLOCK
 test -e ~/.cw/env.sh; and source ~/.cw/env.sh
 # END ANSIBLE MANAGED BLOCK
