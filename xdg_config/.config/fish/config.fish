876a99d7c8320c1790e29e0a6c906b461bd927f8
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2019-07-26 14:16:11 +0200

    Add go bin to PATH

--- xdg_config/.config/fish/config.fish
+++ xdg_config/.config/fish/config.fish
@@ -20,6 +20,7 @@ set -gx LC_ALL en_US.utf-8
 test -d "$HOME/bin";        and set -gx PATH "$HOME/bin" $PATH
 test -d "$HOME/.local/bin"; and set -gx PATH "$HOME/.local/bin" $PATH
 test -d "$HOME/.cargo/bin"; and set -gx PATH "$HOME/.cargo/bin" $PATH
+test -d "$HOME/go/bin";     and set -gx PATH "$HOME/go/bin" $PATH
 # Add project specific paths
 set -gx PATH ".git/safe/../../bin" $PATH
 set -gx PATH ".git/safe/../../node_modules/.bin/" $PATH
