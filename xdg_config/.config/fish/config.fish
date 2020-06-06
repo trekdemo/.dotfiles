64c8d46b5966d0e605da9023d84684f9976db83b
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2020-06-06 20:32:43 +0200

    Install fisher when it's missing

--- xdg_config/.config/fish/config.fish
+++ xdg_config/.config/fish/config.fish
@@ -1,6 +1,13 @@
 # #fisher config
 set -g fisher_path $HOME/.config/fish/fisher-pkg
 
+# Install fisher automatically
+if not functions -q fisher
+  set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+  curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+  fish -c fisher
+end
+
 set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
 set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
 
