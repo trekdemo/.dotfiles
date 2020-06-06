ccf4c0c98829392b9acba8f24f927da973bfa221
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2020-06-06 20:39:24 +0200

    Use install.sh script to store all install steps

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install brew command
+if ! command -v brew >/dev/null; then
+  echo "Installing Homebrew ..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+  export PATH="/usr/local/bin:$PATH"
+fi
+
+if ! command -v stow >/dev/null; then
+  echo 'Install stow'
+  brew install stow
+fi
+echo 'Link dotfiles'
+stow dev executables git tmux xdg_config terminfo
+
+# Install apps & commands via brew
+brew bundle install --global
+
+# Switch to fish shell
+if [[ -x /usr/local/bin/fish ]]; then
+  sudo bash -c "echo '/usr/local/bin/fish' >> /etc/shells"
+  chsh -s /usr/local/bin/fish
+  /usr/local/bin/fish -c 'fisher'
+fi
+
+# TODO: Install terminfo from alacritty
+# sudo tic -e alacritty,alacritty-direct alacritty.info
+
+# TODO: Install tpm as a git submodule
