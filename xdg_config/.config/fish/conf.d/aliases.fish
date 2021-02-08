4fb4c2e83b52afd651587b0ffa9f0834e47c6577
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2021-02-08 20:45:02 +0100

    Use more abbrs instead of aliases

--- xdg_config/.config/fish/conf.d/aliases.fish
+++ xdg_config/.config/fish/conf.d/aliases.fish
@@ -2,25 +2,29 @@
 alias vim='nvim'
 alias :q='exit'
 
-alias mv="mv -iv"
-alias cp="cp -riv"
-alias mkdir="mkdir -vp"
-
 if status --is-interactive
+  abbr --add --global o open
+  abbr --add --global mv mv -iv
+  abbr --add --global cp cp -riv
+  abbr --add --global mkdir mkdir -vp
+  abbr --add --global cc circleci
+  abbr --add --global t task
+  abbr --add --global tsh tasksh
+  # Ruby and friends
   abbr --add --global b bundle
   abbr --add --global be bundle exec
   abbr --add --global bu bundle update
-  abbr --add --global cc circleci
   abbr --add --global berc bundle exec rails console
-  abbr --add --global o open
-  abbr --add --global ts tig status
+  # Docker, Kubernetes, ...
   abbr --add --global k kubectl
-  abbr --add --global kp prod-kubectl
-  abbr --add --global kd dev-kubectl
+  abbr --add --global kx kubectx
+  abbr --add --global kn kubens
   abbr --add --global d docker
   abbr --add --global dc docker-compose
   abbr --add --global ds docker-sync
+  # Git and friends
   abbr --add --global g git
+  abbr --add --global ts tig status
   abbr --add --global gup 'git fetch --all --prune; git pull --rebase'
   abbr --add --global gfp 'git push --force-with-lease'
 
