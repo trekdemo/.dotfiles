9e1a4113856e593661dbb73425ab5daaa36ab3b7
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2021-10-23 23:17:24 +0200

    Add treesitter configuration

---
+++ xdg_config/.config/nvim/after/plugin/treesitter.lua
@@ -0,0 +1,101 @@
+local list = require("nvim-treesitter.parsers").get_parser_configs()
+
+-- :h nvim-treesitter-query-extensions
+local custom_captures = {
+  ["function.call"] = "LuaFunctionCall",
+  ["function.bracket"] = "Type",
+  ["namespace.type"] = "TSNamespaceType",
+}
+
+list.sql = {
+  install_info = {
+    url = "https://github.com/DerekStride/tree-sitter-sql",
+    files = { "src/parser.c" },
+    branch = "main",
+  },
+}
+
+local _ = require("nvim-treesitter.configs").setup {
+  ensure_installed = { "ruby", "yaml", "sql", "go", "query", "html", "css", "lua", "vim", "bash" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = false,
+    disable = { "json" },
+    custom_captures = custom_captures,
+  },
+
+  refactor = {
+    highlight_definitions = { enable = true },
+    highlight_current_scope = { enable = false },
+
+    smart_rename = {
+      enable = false,
+      keymaps = {
+        -- mapping to rename reference under cursor
+        smart_rename = "grr",
+      },
+    },
+
+    navigation = {
+      enable = false,
+    },
+  },
+
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+
+  context_commentstring = {
+    enable = true,
+    config = {
+      ruby = "# %s",
+      c = "// %s",
+      lua = "-- %s",
+    },
+  },
+
+  textobjects = {
+    move = {
+      enable = true,
+      set_jumps = true,
+
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+
+    select = {
+      enable = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+
+        ["ac"] = "@conditional.outer",
+        ["ic"] = "@conditional.inner",
+
+        ["aa"] = "@parameter.outer",
+        ["ia"] = "@parameter.inner",
+      },
+    },
+  },
+}
