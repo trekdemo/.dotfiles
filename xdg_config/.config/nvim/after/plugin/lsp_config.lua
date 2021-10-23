f7b3f0113ae18c625d1274c2c1bf5199a263da07
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2021-10-23 21:31:16 +0200

    Extract completion and lsp config to after/plugin

--- xdg_config/.config/nvim/after/plugin/lsp_config.lua
+++ xdg_config/.config/nvim/after/plugin/lsp_config.lua
@@ -92,3 +92,10 @@ lsp.yamlls.setup({
 --     }
 --   }
 -- }
+
+vim.cmd [[
+  sign define LspDiagnosticsSignError text=ﱥ texthl=LspDiagnosticsSignError linehl= numhl=
+  sign define LspDiagnosticsSignWarning text= texthl=LspDiagnosticsSignWarning linehl= numhl=
+  sign define LspDiagnosticsSignInformation text= texthl=LspDiagnosticsSignInformation linehl= numhl=
+  sign define LspDiagnosticsSignHint text= texthl=LspDiagnosticsSignHint linehl= numhl=
+]]
