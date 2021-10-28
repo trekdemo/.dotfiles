78685a63e4b3099a4ea0a103ffa54f8a6d9f17fa
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2021-10-28 19:22:53 +0200

    Extract config for nvim-tmux-navigator

---
+++ xdg_config/.config/nvim/lua/config/nvim-tmux-navigator.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+local options = { noremap = true, silent = true }
+local map = vim.api.nvim_set_keymap
+
+M.config = function()
+  require'nvim-tmux-navigation'.setup {
+    disable_when_zoomed = false -- Default: false
+  }
+  map('n', "<C-h>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLeft()<cr>", options)
+  map('n', "<C-j>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateDown()<cr>", options)
+  map('n', "<C-k>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateUp()<cr>", options)
+  map('n', "<C-l>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateRight()<cr>", options)
+  -- vim.api.nvim_set_keymap('n', "<C-\\>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLastActive()<cr>", { noremap = true, silent = true })
+  -- vim.api.nvim_set_keymap('n', "<C-Space>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateNext()<cr>", { noremap = true, silent = true })
+  map('t', "<C-h>", "<C-\\><C-n><C-h>", { silent = true })
+  map('t', "<C-j>", "<C-\\><C-n><C-j>", { silent = true })
+  map('t', "<C-k>", "<C-\\><C-n><C-k>", { silent = true })
+  -- " I use <C-l> too much to clear the terminal
+  -- map('t', "<C-l>", "<C-\\><C-n><C-l>", options)
+end
+
+return M
--- xdg_config/.config/nvim/lua/plugins.lua
+++ xdg_config/.config/nvim/lua/plugins.lua
@@ -73,33 +73,14 @@ return require('packer').startup(function(use)
   use 'kana/vim-smartinput' -- Help with pairs of punctuations such as (), [], {}, '', "" and so on.
   use 'ggandor/lightspeed.nvim' -- Super-powers for the s, S, f, t operators
 
+  use 'tmux-plugins/vim-tmux' -- Plugin for .tmux.conf ftplugin
   if vim.env.TMUX then
-    use 'tmux-plugins/vim-tmux' -- Plugin for .tmux.conf ftplugin
     use {
       'alexghergh/nvim-tmux-navigation',
-      config = function()
-        require'nvim-tmux-navigation'.setup {
-          disable_when_zoomed = false -- Default: false
-        }
-
-        local options = { noremap = true, silent = true }
-        local map = vim.api.nvim_set_keymap
-        map('n', "<C-h>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLeft()<cr>", options)
-        map('n', "<C-j>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateDown()<cr>", options)
-        map('n', "<C-k>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateUp()<cr>", options)
-        map('n', "<C-l>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateRight()<cr>", options)
-        -- vim.api.nvim_set_keymap('n', "<C-\\>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLastActive()<cr>", { noremap = true, silent = true })
-        -- vim.api.nvim_set_keymap('n', "<C-Space>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateNext()<cr>", { noremap = true, silent = true })
-        map('t', "<C-h>", "<C-\\><C-n><C-h>", { silent = true })
-        map('t', "<C-j>", "<C-\\><C-n><C-j>", { silent = true })
-        map('t', "<C-k>", "<C-\\><C-n><C-k>", { silent = true })
-        -- " I use <C-l> too much to clear the terminal
-        -- map('t', "<C-l>", "<C-\\><C-n><C-l>", options)
-      end
+      config = require('config/nvim-tmux-navigator').config,
     }
   end
 
-  -- " Running tests
   use {
     'janko/vim-test',
     requires = {
