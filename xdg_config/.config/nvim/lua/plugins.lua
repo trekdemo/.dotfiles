2ce6c56e57123c2f594af4ad16859d3960c42bd9
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2021-11-17 14:07:17 +0100

    Highlight the netrw buffers (background)

--- xdg_config/.config/nvim/lua/plugins.lua
+++ xdg_config/.config/nvim/lua/plugins.lua
@@ -54,6 +54,7 @@ return require('packer').startup(function(use)
         },
 
         contrast_windows = { -- Specify which windows get the contrasted (darker) background
+          'netrw',
           'help',
           'terminal',
           'packer',
