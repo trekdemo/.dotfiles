61f1b9e0fb39bef54382fb632333e5a1c42fe523
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2021-11-20 18:47:54 +0100

    Load vimwiki config upfront

--- xdg_config/.config/nvim/after/ftplugin/markdown.lua
+++ xdg_config/.config/nvim/after/ftplugin/markdown.lua
@@ -1,8 +1,6 @@
 vim.g.markdown_fenced_languages = { 'yaml', 'ruby', 'json', 'sh', 'javascript' }
 
 local util = require('utils')
-util.nmap('<localleader>p', ":!open -a 'Marked 2' '%'<cr>", { buffer = true, silent = true })
-
 local cmp = util.prequire("cmp")
 if cmp then
   cmp.setup.buffer {
--- xdg_config/.config/nvim/after/ftplugin/markdown.vim
+++ xdg_config/.config/nvim/after/ftplugin/markdown.vim
@@ -2,3 +2,5 @@ setlocal spell spelllang=en,hu
 setlocal wrap linebreak
 setlocal breakindent breakindentopt=shift:2
 setlocal formatoptions=ln
+
+nnoremap <buffer> <silent> gP :!open -a 'Marked 2' '%'<cr>
