a20674654609c252d87e864d6dcf39b354779713
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2021-11-20 17:54:31 +0100

    Simplify bufferline filter function

--- xdg_config/.config/nvim/lua/plugins.lua
+++ xdg_config/.config/nvim/lua/plugins.lua
@@ -78,11 +78,9 @@ require('packer').startup(function(use)
           max_name_length = 30,
           show_close_icon = false,
           custom_filter = function(buf)
-            local ignored_ft = { 'help', 'fugitive', 'term', 'neoterm' }
-            for _, ft in ipairs(ignored_ft) do
-              if ft == vim.bo[buf].filetype then
-                return false
-              end
+            local ignored_ft = { 'help', 'fugitive', 'term', 'neoterm', 'netrw' }
+            if vim.tbl_contains(ignored_ft, vim.bo[buf].filetype) then
+              return false
             end
 
             return true
