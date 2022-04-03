bb8643dd631f4eb2eac0b413db7fc45040a8c2c4
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-04-03 18:30:44 +0200

    refactor(lsp): Extract functions for better readability

--- xdg_config/.config/nvim/lua/geri/config/nvim-lspconfig.lua
+++ xdg_config/.config/nvim/lua/geri/config/nvim-lspconfig.lua
@@ -1,37 +1,49 @@
 local M = {}
 
-function M.config ()
-  -- Here's the list of available LSP servers
-  -- https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md
-  local lsp = require('lspconfig')
-  local options = {
+local defaultOptions = function ()
+  return {
     on_attach = require('geri.config.lsp-on-attach'),
     flags = {
       -- This will be the default in neovim 0.7+
       debounce_text_changes = 150,
     },
   }
+end
 
-  local servers = { 'vimls', 'bashls', 'cssls', 'solargraph', 'dockerls', 'gopls', 'html', 'jsonls', 'pylsp', 'terraformls', 'tsserver', 'yamlls' }
-  for _, server in pairs(servers) do
-    lsp[server].setup(options)
-  end
-
-  -- Configure Lua
+local luaDevOptions = function()
+  local lsp = require('lspconfig')
   local sumneko_root_path = vim.fn.stdpath('cache')..'/lua-language-server'
   local sumneko_binary = sumneko_root_path.."/bin/macOS/lua-language-server"
-  local luadev = require("lua-dev").setup({
-    lspconfig = vim.tbl_deep_extend('keep', options, {
-      cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
-      root_dir = lsp.util.root_pattern('.nvim-root')
-    })
+
+  return require("lua-dev").setup({
+    lspconfig = vim.tbl_deep_extend(
+      'keep',
+      defaultOptions(),
+      {
+        cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+        root_dir = lsp.util.root_pattern('.nvim-root')
+      }
+    )
   })
-  lsp.sumneko_lua.setup(luadev)
+end
+
+function M.config ()
+  local lsp = require('lspconfig')
+
+  -- Here's the list of available LSP servers
+  -- https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md
+  local servers = { 'vimls', 'bashls', 'cssls', 'solargraph', 'dockerls', 'gopls', 'html', 'jsonls', 'pylsp', 'terraformls', 'tsserver', 'yamlls' }
+  for _, server in pairs(servers) do
+    lsp[server].setup(defaultOptions())
+  end
+  -- Lua gets some additional fields
+  lsp.sumneko_lua.setup(luaDevOptions())
 
   vim.fn.sign_define("DiagnosticSignError", { text = "✗", texthl = "DiagnosticSignError" })
   vim.fn.sign_define("DiagnosticSignWarn",  { text = "", texthl = "DiagnosticSignWarn" })
   vim.fn.sign_define("DiagnosticSignInfo",  { text = "", texthl = "DiagnosticSignInfo" })
   vim.fn.sign_define("DiagnosticSignHint",  { text = "", texthl = "DiagnosticSignHint" })
 end
+
 
 return M
