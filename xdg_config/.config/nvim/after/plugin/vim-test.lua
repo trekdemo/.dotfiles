cd9c707078305acf6afd09f6e0234645f08f9051
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-04-03 18:42:31 +0200

    Rewrite vim-test config in lua

---
+++ xdg_config/.config/nvim/after/plugin/vim-test.lua
@@ -0,0 +1,20 @@
+vim.cmd [[
+let test#strategy = 'dispatch'
+let test#ruby#rspec#options = '--format progress'
+]]
+
+require('which-key').register({
+  t = {
+    name = '+test',
+    ['<C-l>'] = {'<cmd>TestLast<cr>', 'Run last test'},
+    ['<C-g>'] = {'<cmd>TestLast<cr>', 'Go to last test'},
+
+    ['<C-n>'] = {'<cmd>TestNearest<cr>', 'Test nearest'},
+    ['<C-f>'] = {'<cmd>TestFile<cr>', 'Test file'},
+    ['<C-s>'] = {'<cmd>TestSuite<cr>', 'Test suite'},
+
+    ['<S-n>'] = {'<cmd>TestNearest -strategy=vimux<cr>', 'Test nearest'},
+    ['<S-f>'] = {'<cmd>TestFile -strategy=vimux<cr>', 'Test file'},
+    ['<S-s>'] = {'<cmd>TestSuite -strategy=vimux<cr>', 'Test ruite'},
+  }
+}, {silent = true})
