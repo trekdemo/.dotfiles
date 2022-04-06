41e57625a1496ac14e9de141e3d00c96f4c7a23c
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-04-06 16:31:23 +0200

    Use less themes with Telescope
    After some digging in its source, I realized that I don't really need 3
    different themes. I like the ivy a.k.a. bottom_pane theme/strategy.
    
    I also found out that there are toggle functions to toggle the preview.

--- xdg_config/.config/nvim/after/plugin/telescope.lua
+++ xdg_config/.config/nvim/after/plugin/telescope.lua
@@ -1,12 +1,19 @@
 local actions = require("telescope.actions")
+local action_layout = require("telescope.actions.layout")
 local telescope = require('telescope')
 telescope.setup({
   defaults = {
-    layout_strategy = 'horizontal',
+    layout_strategy = 'bottom_pane',
+    sorting_strategy = 'ascending',
+    layout_config = { },
+    borderchars = {'▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' },
+    previewer = false,
+    winblend = 15,
     mappings = {
       i = {
         ["<esc>"] = actions.close,
         ['<c-d>'] = actions.delete_buffer,
+        ['<C-p>'] = action_layout.toggle_preview,
       },
     },
   }
@@ -17,60 +24,17 @@ telescope.load_extension('vim_docs')
 -- ----------------------------------------------------------------------------
 -- Themes
 -- ----------------------------------------------------------------------------
-local themes = require('telescope.themes')
-local theme_defaults = {
-  borderchars = {
-    prompt = {'▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' },
-    results = {'▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' },
-    preview = {'▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' },
-  },
-  results_title = '',
-  preview_title = '',
-  winblend = 15,
-}
-local full = function(opt)
-  opt = opt or {}
-  return themes.get_dropdown(
-    vim.tbl_deep_extend('force', theme_defaults, {
-      width = 0.8,
-      show_line = false,
-      layout_strategy = 'horizontal',
-      layout_config = {
-        width = 0.8,
-        height = 0.9,
-        prompt_position = "top",
-        preview_cutoff = 120,
-      }
-    }, opt)
-  )
-end
-local cursor = function()
-  return themes.get_cursor(
-    vim.tbl_deep_extend('force', theme_defaults, {
+local cursor = function(opt)
+  return require('telescope.themes').get_cursor(
+    vim.tbl_deep_extend('force', {
       layout_config = { width = 60 },
-      previewer = false,
-      prompt_title = false
-    })
-  )
-end
-local center = function(opt)
-  opt = opt or {}
-  return themes.get_dropdown(
-    vim.tbl_deep_extend('force', theme_defaults, {
-      layout_strategy = 'center',
-      layout_config = {
-        width = 0.8,
-        height = 0.5,
+      borderchars = {
+        prompt = {'▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' },
+        results = {' ', '▐', '▄', '▌', '▌', '▐', '▟', '▙' },
       },
       previewer = false,
-      prompt_title = false
-    }, opt)
-  )
-end
-local ivy = function(opt)
-  opt = opt or {}
-  return themes.get_ivy(
-    vim.tbl_deep_extend('force', theme_defaults, opt)
+      results_title = '',
+    }, opt or {})
   )
 end
 
@@ -79,23 +43,23 @@ end
 -- ----------------------------------------------------------------------------
 local builtins = require('telescope.builtin')
 require('which-key').register({
-  ['<C-p>'] = { function() builtins.find_files(full()) end, "Find files" },
-  ['<C-b>'] = { function() builtins.buffers(full()) end, "Buffers" },
-  ['<M-b>'] = { function() builtins.builtin(center()) end, "Builtins" },
+  ['<C-p>'] = { function() builtins.find_files() end, "Find files" },
+  ['<C-b>'] = { function() builtins.buffers() end, "Buffers" },
+  ['<M-b>'] = { function() builtins.builtin() end, "Builtins" },
   ['<leader>f'] = {
     name = "+telescope",
     a = { function() builtins.lsp_code_actions(cursor()) end, "LSP: Code Actions" },
-    c = { function() builtins.commands(full()) end, "Commands" },
+    c = { function() builtins.commands() end, "Commands" },
     e = { function() builtins.symbols(cursor()) end, "Emojies" },
-    g = { function() builtins.git_status(ivy()) end, "Git Status" },
-    h = { function() builtins.help_tags(full()) end, "Help Tags" },
-    m = { function() builtins.keymaps(ivy()) end, "Keymaps" },
-    t = { function() builtins.treesitter(center()) end, "TreeSitter" },
-    v = { function() builtins.find_files(full({cwd="~/.config/nvim"})) end, "NVim Configuration" },
-    r = { function() builtins.lsp_references(ivy()) end, 'LSP: References'},
-    d = { function() builtins.diagnostics(ivy()) end, 'Diagnostics'},
+    g = { function() builtins.git_status() end, "Git Status" },
+    h = { function() builtins.help_tags() end, "Help Tags" },
+    m = { function() builtins.keymaps() end, "Keymaps" },
+    t = { function() builtins.treesitter(cursor({layout_config = {height = 15}})) end, "TreeSitter" },
+    v = { function() builtins.find_files({cwd="~/.config/nvim"}) end, "NVim Configuration" },
+    r = { function() builtins.lsp_references() end, 'LSP: References'},
+    d = { function() builtins.diagnostics() end, 'Diagnostics'},
   },
-  ['gr'] = { function() builtins.lsp_references(ivy()) end, 'LSP: References'},
+  ['gr'] = { function() builtins.lsp_references() end, 'LSP: References'},
 }, {noremap = true})
 
 vim.cmd [[
