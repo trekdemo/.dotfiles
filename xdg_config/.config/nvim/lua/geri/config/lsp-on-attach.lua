8475a7802742c8b6351bc70cd12b5adcd7b72406
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-04-27 17:21:33 +0200

    Rewrite LSP aucmds using lua

--- xdg_config/.config/nvim/lua/geri/config/lsp-on-attach.lua
+++ xdg_config/.config/nvim/lua/geri/config/lsp-on-attach.lua
@@ -1,3 +1,35 @@
+local activateLspFormatting = function()
+  local lsp_format_grp = vim.api.nvim_create_augroup("MyLspFormating", {clear = true})
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = lsp_format_grp,
+    buffer = 0,
+    callback = vim.lsp.buf.formatting_sync
+  })
+end
+
+local activateLspReferenceUnderCursor = function ()
+  local lsp_ref_grp = vim.api.nvim_create_augroup("MyLspReferenceUnderCursor", {clear = true})
+  vim.api.nvim_create_autocmd("CursorHold", {
+    group = lsp_ref_grp,
+    buffer = 0,
+    callback = vim.lsp.buf.document_highlight,
+  })
+  vim.api.nvim_create_autocmd("CursorMoved", {
+    group = lsp_ref_grp,
+    buffer = 0,
+    callback = vim.lsp.buf.clear_references,
+  })
+end
+
+local activateDiagnosticFloat = function()
+  local grp = vim.api.nvim_create_augroup("MyDiagnosticFloat", {clear = true})
+  vim.api.nvim_create_autocmd("CursorHold", {
+    group = grp,
+    buffer = 0,
+    callback = function () vim.diagnostic.open_float({focusable = false}) end
+  })
+end
+
 return function(client, bufnr)
     require('which-key').register({
       ['g0'] = {'<cmd>lua vim.lsp.buf.document_symbol()<CR>', 'LSP: Document Symbol'},
@@ -19,35 +51,13 @@ return function(client, bufnr)
       [']d']         = { '<cmd>lua vim.diagnostic.goto_next()<CR>', 'Diagnostics: Next' },
     }, { noremap = true, buffer = bufnr })
 
-    local end_message = "LSP client started (" .. client.name .. "):"
+    activateDiagnosticFloat()
     if client.resolved_capabilities.document_formatting then
-      end_message = end_message .. " formatting"
-
-      vim.cmd([[
-      augroup LspFormatting
-          autocmd! * <buffer>
-          autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-      augroup END
-      ]])
+      activateLspFormatting()
     end
-
-    -- puts(client.resolved_capabilities)
     if client.resolved_capabilities.document_highlight then
-      vim.cmd([[
-      augroup CustomLspHighlighting
-        autocmd! * <buffer>
-        autocmd! CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd! CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-      ]])
+      activateLspReferenceUnderCursor()
     end
-
-    vim.cmd([[
-    augroup CustomDiagnostics
-      autocmd! * <buffer>
-      autocmd CursorHold <buffer> lua vim.diagnostic.open_float({focusable = false})
-    augroup END
-    ]])
 
 
     vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
@@ -55,5 +65,5 @@ return function(client, bufnr)
     vim.api.nvim_win_set_option(0, 'foldmethod', 'expr')
     vim.api.nvim_win_set_option(0, 'foldexpr', 'nvim_treesitter#foldexpr()')
 
-    print(end_message);
+    print("LSP client started (" .. client.name .. ")");
   end
