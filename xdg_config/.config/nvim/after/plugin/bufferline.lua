d1adba3af81796e8952ec9b963b956e94f49e257
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-04-27 20:18:22 +0200

    Show all the buffers in the tabline in the first tab

--- xdg_config/.config/nvim/after/plugin/bufferline.lua
+++ xdg_config/.config/nvim/after/plugin/bufferline.lua
@@ -11,6 +11,12 @@ require('bufferline').setup({
     max_name_length = 30,
     show_close_icon = false,
     custom_filter = function(buf)
+      local curr_tab_id = vim.fn.tabpagenr()
+      local tab_count = vim.fn.tabpagenr('$')
+
+      if curr_tab_id == 1 or tab_count == 1 then
+        return true
+      end
       -- Only show buffers in the current tab
       if vim.tbl_contains(vim.fn.tabpagebuflist(), buf) then
         return true
