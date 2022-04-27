faef58c4ba9f9489a7fc3ece0adcb75e2937c4f1
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-04-27 17:42:40 +0200

    Simplify the testing keymaps
    Adding an extra key is not necessary. Without <C> the mapping is still
    uniq, so I can spare that keypress.

--- xdg_config/.config/nvim/after/plugin/vim-test.lua
+++ xdg_config/.config/nvim/after/plugin/vim-test.lua
@@ -8,10 +8,15 @@ require('which-key').register({
     name = '+test',
     ['<C-l>'] = {'<cmd>TestLast<cr>', 'Run last test'},
     ['<C-g>'] = {'<cmd>TestLast<cr>', 'Go to last test'},
+    ['l'] = {'<cmd>TestLast<cr>', 'Run last test'},
+    ['g'] = {'<cmd>TestLast<cr>', 'Go to last test'},
 
     ['<C-n>'] = {'<cmd>TestNearest<cr>', 'Test nearest'},
     ['<C-f>'] = {'<cmd>TestFile<cr>', 'Test file'},
     ['<C-s>'] = {'<cmd>TestSuite<cr>', 'Test suite'},
+    ['n'] = {'<cmd>TestNearest<cr>', 'Test nearest'},
+    ['f'] = {'<cmd>TestFile<cr>', 'Test file'},
+    ['s'] = {'<cmd>TestSuite<cr>', 'Test suite'},
 
     ['<S-n>'] = {'<cmd>TestNearest -strategy=vimux<cr>', 'Test nearest'},
     ['<S-f>'] = {'<cmd>TestFile -strategy=vimux<cr>', 'Test file'},
