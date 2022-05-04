9da6402572a0e039b7322e6cd24a553c74de264c
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-05-04 08:51:30 +0200

    Add mapping to access todo comments with telescope

--- xdg_config/.config/nvim/lua/plugins.lua
+++ xdg_config/.config/nvim/lua/plugins.lua
@@ -256,11 +256,8 @@ require('packer').startup(function(use)
     "folke/todo-comments.nvim",
     requires = "nvim-lua/plenary.nvim",
     config = function()
-      require("todo-comments").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
+      require("todo-comments").setup { }
+      vim.keymap.set('n', '<leader>ft', ':TodoTelescope<CR>')
     end
   }
 
