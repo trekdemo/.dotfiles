218b7af7b4b68ebdbab04941e0c7bab838d115bb
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-05-10 10:14:03 +0200

    Move test mappings under <leader>t prefix
    This way I'm not hiding any characters when jumping with t

--- xdg_config/.config/nvim/after/plugin/vim-test.lua
+++ xdg_config/.config/nvim/after/plugin/vim-test.lua
@@ -4,22 +4,18 @@ let test#ruby#rspec#options = '--format progress'
 ]]
 
 require('which-key').register({
-  t = {
-    name = '+test',
-    ['<C-l>'] = {'<cmd>TestLast<cr>', 'Run last test'},
-    ['<C-g>'] = {'<cmd>TestLast<cr>', 'Go to last test'},
-    ['l'] = {'<cmd>TestLast<cr>', 'Run last test'},
-    ['g'] = {'<cmd>TestLast<cr>', 'Go to last test'},
+  ['<leader>'] = {
+    t = {
+      name = '+test',
+      l = {'<cmd>TestLast<cr>', 'Run last test'},
 
-    ['<C-n>'] = {'<cmd>TestNearest<cr>', 'Test nearest'},
-    ['<C-f>'] = {'<cmd>TestFile<cr>', 'Test file'},
-    ['<C-s>'] = {'<cmd>TestSuite<cr>', 'Test suite'},
-    ['n'] = {'<cmd>TestNearest<cr>', 'Test nearest'},
-    ['f'] = {'<cmd>TestFile<cr>', 'Test file'},
-    ['s'] = {'<cmd>TestSuite<cr>', 'Test suite'},
+      n = {'<cmd>TestNearest<cr>', 'Run the nearest test'},
+      f = {'<cmd>TestFile<cr>', 'Run test for the current file'},
+      s = {'<cmd>TestSuite<cr>', 'Run test of the project'},
 
-    ['<S-n>'] = {'<cmd>TestNearest -strategy=vimux<cr>', 'Test nearest'},
-    ['<S-f>'] = {'<cmd>TestFile -strategy=vimux<cr>', 'Test file'},
-    ['<S-s>'] = {'<cmd>TestSuite -strategy=vimux<cr>', 'Test ruite'},
-  }
+      N = {'<cmd>TestNearest -strategy=vimux<cr>', 'Run the nearest test in tmux'},
+      F = {'<cmd>TestFile -strategy=vimux<cr>', 'Run test for the current file in tmux'},
+      S = {'<cmd>TestSuite -strategy=vimux<cr>', 'Run test of the project in tmux'},
+    }
+  },
 }, {silent = true})
