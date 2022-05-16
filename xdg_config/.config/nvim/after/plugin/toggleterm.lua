0af452b5721184e98f84c44081637e777048f5fb
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-05-16 20:23:59 +0200

    Reduce the configuration of toggleterm

--- xdg_config/.config/nvim/after/plugin/toggleterm.lua
+++ xdg_config/.config/nvim/after/plugin/toggleterm.lua
@@ -1,24 +1,20 @@
 require("toggleterm").setup({
   open_mapping = [[<M-t>]],
   direction = 'float',
-  shade_terminals = false,
   float_opts = {
     border = {'▛', '▀','▜', '▐', '▟', '▄', '▙', '▌'},
   },
-  on_open = function(term)
-    vim.keymap.set('t', "jk", [[<C-\><C-n>]], {buffer = term.bufnr})
-  end
 })
+
+vim.cmd [[tnoremap <A-[> <c-\><c-n>]]
 
 local Terminal  = require('toggleterm.terminal').Terminal
 
 local gh_dash = Terminal:new({ cmd = "gh dash", hidden = true })
-vim.keymap.set("n", "<M-S-h>", function() gh_dash:toggle() end)
+vim.keymap.set({'n', 't'}, "<M-S-h>", function() gh_dash:toggle() end)
 
 local tig = Terminal:new({ cmd = "tig", hidden = true })
-vim.keymap.set("n", "<M-S-g>", function() tig:toggle() end)
+vim.keymap.set({'n', 't'}, "<M-S-g>", function() tig:toggle() end)
 
-  -- highlight clear ToggleTermFloatBorder
-  -- highlight clear ToggleTermNormalFloat
-  -- highlight link ToggleTermFloatBorder FloatBorder
-  -- highlight link ToggleTermNormalFloat FloatBorder
+local rails_c = Terminal:new({ cmd = "bin/rails console", hidden = true, close_on_exit = false })
+vim.keymap.set({'n', 't'}, "<M-S-r>", function() rails_c:toggle() end)
