05a682ac1f5b69429190248cf2fec4b47ab336e2
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-05-17 19:46:33 +0200

    Use horizontal as the default terminal layout

--- xdg_config/.config/nvim/after/plugin/toggleterm.lua
+++ xdg_config/.config/nvim/after/plugin/toggleterm.lua
@@ -1,8 +1,10 @@
 require("toggleterm").setup({
   open_mapping = [[<M-t>]],
-  direction = 'float',
+  direction = 'horizontal',
+  size = 25,
   float_opts = {
-    border = {'▛', '▀','▜', '▐', '▟', '▄', '▙', '▌'},
+    -- border = {'▛', '▀','▜', '▐', '▟', '▄', '▙', '▌'},
+    border = 'rounded',
   },
 })
 
@@ -10,17 +12,16 @@ vim.cmd [[tnoremap <A-[> <c-\><c-n>]]
 
 local Terminal  = require('toggleterm.terminal').Terminal
 
-local gh_dash = Terminal:new({ cmd = "gh dash", hidden = true })
+local gh_dash = Terminal:new({ cmd = "gh dash", hidden = true, direction = 'tab' })
 vim.keymap.set({'n', 't'}, "<M-S-h>", function() gh_dash:toggle() end)
 
-local tig = Terminal:new({ cmd = "tig", hidden = true })
+local tig = Terminal:new({ cmd = "tig", hidden = true, direction = 'float' })
 vim.keymap.set({'n', 't'}, "<M-S-g>", function() tig:toggle() end)
 
 local rails_c = Terminal:new({
   cmd = "bin/rails console",
   dir = "git_dir",
   direction = 'horizontal',
-  hidden = true,
   close_on_exit = false,
 })
 vim.keymap.set({'n', 't'}, "<M-S-r>", function() rails_c:toggle() end)
