7f0d63ad2d610a8cea44bb5bc23b1b1c333f3d03
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-05-21 20:46:35 +0200

    Remove unused bufferline mappings

--- xdg_config/.config/nvim/after/plugin/bufferline.lua
+++ xdg_config/.config/nvim/after/plugin/bufferline.lua
@@ -1,10 +1,5 @@
-local util = require('utils')
-util.nnoremap('gb', ':BufferLinePick<CR>')
-util.nnoremap('<leader>bd', ':BufferLinePickClose<CR>')
-util.nnoremap('<leader>bD', ':bdelete<CR>')
-
 vim.cmd('command! BufOnly silent! execute "%bd|e#|bd#"')
-util.nnoremap('<leader>bo', ':BufOnly<CR>')
+vim.keymap.set('n', '<leader>bo', ':BufOnly<CR>')
 
 require('bufferline').setup({
   options = {
