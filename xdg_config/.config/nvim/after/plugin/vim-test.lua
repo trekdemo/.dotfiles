1187ba8bd96fa5c7822abdb4ad3c9ba83287a197
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-05-21 20:54:22 +0200

    Update vim-test mappings

--- xdg_config/.config/nvim/after/plugin/vim-test.lua
+++ xdg_config/.config/nvim/after/plugin/vim-test.lua
@@ -7,15 +7,12 @@ require('which-key').register({
   ['<leader>'] = {
     t = {
       name = '+test',
-      l = {'<cmd>TestLast<cr>', 'Run last test'},
+      l = {'<cmd>TestLast<cr>', 'Re-run test'},
+      a = {'<cmd>AbortDispatch<cr>', 'Abort dispatch'},
 
-      n = {'<cmd>TestNearest<cr>', 'Run the nearest test'},
-      f = {'<cmd>TestFile<cr>', 'Run test for the current file'},
-      s = {'<cmd>TestSuite<cr>', 'Run test of the project'},
-
-      N = {'<cmd>TestNearest -strategy=vimux<cr>', 'Run the nearest test in tmux'},
-      F = {'<cmd>TestFile -strategy=vimux<cr>', 'Run test for the current file in tmux'},
-      S = {'<cmd>TestSuite -strategy=vimux<cr>', 'Run test of the project in tmux'},
+      n = {'<cmd>TestNearest<cr>', 'Test nearest'},
+      f = {'<cmd>TestFile<cr>', 'Test current file'},
+      s = {'<cmd>TestSuite<cr>', 'Test suite'},
     }
   },
 }, {silent = true})
