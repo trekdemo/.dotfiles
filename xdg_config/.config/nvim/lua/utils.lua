26631badb73ab7848401ff4c3dc917c7d19f69d3
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-05-26 21:09:58 +0200

    Use vim.pretty_print instead of custom func

--- xdg_config/.config/nvim/lua/utils.lua
+++ xdg_config/.config/nvim/lua/utils.lua
@@ -1,13 +1,5 @@
-_G.puts = function (...)
-  local objects = {}
-  for i = 1, select('#', ...) do
-    local v = select(i, ...)
-    table.insert(objects, vim.inspect(v))
-  end
-
-  print(table.concat(objects, '\n'))
-  return ...
-end
+_G.puts = vim.pretty_print
+vim.cmd [[cabbrev luap lua vim.pretty_print()<Left>]]
 
 local M = {}
 
