b0791563d84269ed56f1a489b21fb9096803d918
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-12-22 20:06:19 +0100

    Make nnn more useful
    - Setup better preview
    - Setup lazy aliases
    - Only open on Enter

--- home/.config/zsh/conf.d/nnn.zsh
+++ home/.config/zsh/conf.d/nnn.zsh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-# export NNN_OPTS="de"
-alias ls="nnn -de"
+alias n="nnn"
+alias nn="nnn"
+
+# -d detail mode
+# -e text in $VISUAL/$EDITOR/vi
+# -o open files only on Enter
+# -U show user and group
+export NNN_OPTS="deoU"
 export NNN_PLUG="g:gitroot;c:fzcd;p:preview-tui"
 export NNN_FIFO=/tmp/nnn.fifo nnn
+export NNN_OPENER=~/.config/nnn/plugins/nuke
