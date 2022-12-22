bab1b7cb9762f61ab53e0165f8905daf17bd7473
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-12-22 20:32:19 +0100

    Add additional fugitive mappings

--- home/.config/nvim/after/plugin/fugitive.lua
+++ home/.config/nvim/after/plugin/fugitive.lua
@@ -9,3 +9,27 @@ vim.keymap.set('n', '<leader>ga', vim.cmd.Gadd)
 vim.keymap.set('n', '<leader>gb', function() vim.cmd.Git('blame') end)
 vim.keymap.set('n', '<leader>gl', function() vim.cmd.Git('log') end)
 vim.keymap.set('n', '<leader>gP', ':!gh pr view -w || gh pr create -w<cr>')
+
+local augrp = vim.api.nvim_create_augroup("FugitiveUserMappings", {})
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = augrp,
+  pattern = "*",
+  callback = function()
+    if vim.bo.ft ~= "fugitive" then
+      return
+    end
+
+    local bufnr = vim.api.nvim_get_current_buf()
+
+    vim.keymap.set("n", "g<space>", ':Git ', { buffer = bufnr, remap = false, desc = "Git " })
+
+    vim.keymap.set("n", "<leader>p", function()
+      vim.cmd.Git('push')
+    end, { buffer = bufnr, remap = false, desc = "Git push" })
+
+    -- rebase always
+    vim.keymap.set("n", "<leader>P", function()
+      vim.cmd.Git('pull --rebase')
+    end, { buffer = bufnr, remap = false, desc = "Git pull --rebase" })
+  end,
+})
