ef5cf0c20ee0a9c9b2c1fd117b7a64801226106e
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2023-01-04 20:27:09 +0100

    Revert back to vim.cmd()
    It did not notice it, but vim.cmd.func() is only available in the 0.9.x.

--- home/.config/nvim/after/plugin/fugitive.lua
+++ home/.config/nvim/after/plugin/fugitive.lua
@@ -2,12 +2,12 @@ local vim = vim
 vim.cmd.cabbrev('g', 'Git')
 
 vim.keymap.set('n', '<leader>g<Space>', ':Git<Space>')
-vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
-vim.keymap.set('n', '<leader>gg', vim.cmd.Git)
-vim.keymap.set('n', '<leader>gw', vim.cmd.Gwrite)
-vim.keymap.set('n', '<leader>ga', vim.cmd.Gadd)
-vim.keymap.set('n', '<leader>gb', function() vim.cmd.Git('blame') end)
-vim.keymap.set('n', '<leader>gl', function() vim.cmd.Git('log') end)
+vim.keymap.set('n', '<leader>gs', ':Git<cr>')
+vim.keymap.set('n', '<leader>gg', ':Git<cr>')
+vim.keymap.set('n', '<leader>gw', ':Gwrite<cr>')
+vim.keymap.set('n', '<leader>ga', ':Gadd<cr>')
+vim.keymap.set('n', '<leader>gb', ':Git blame')
+vim.keymap.set('n', '<leader>gl', ':Git log')
 vim.keymap.set('n', '<leader>gP', ':!gh pr view -w || gh pr create -w<cr>')
 
 local augrp = vim.api.nvim_create_augroup("FugitiveUserMappings", {})
@@ -24,12 +24,12 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
     vim.keymap.set("n", "g<space>", ':Git ', { buffer = bufnr, remap = false, desc = "Git " })
 
     vim.keymap.set("n", "<leader>p", function()
-      vim.cmd.Git('push')
+      vim.cmd ':Git push'
     end, { buffer = bufnr, remap = false, desc = "Git push" })
 
     -- rebase always
     vim.keymap.set("n", "<leader>P", function()
-      vim.cmd.Git('pull --rebase')
+      vim.cmd ':Git pull --rebase'
     end, { buffer = bufnr, remap = false, desc = "Git pull --rebase" })
   end,
 })
