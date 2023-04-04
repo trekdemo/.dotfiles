4f5cb7163d342dac3f4f5dd6dac5c81f0ce6e93d
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2023-04-04 09:01:15 +0200

    Add <space>tD to toggle background test execution

--- home/.config/nvim/after/plugin/vim-test.lua
+++ home/.config/nvim/after/plugin/vim-test.lua
@@ -2,7 +2,6 @@ vim.g['test#ruby#rspec#options'] = '--format progress'
 vim.g['test#strategy'] = 'dispatch'
 
 vim.keymap.set('n', '<leader>tl', '<Cmd>TestLast<CR>')
-vim.keymap.set('n', '<leader>tt', '<Cmd>TestLast<CR>')
 vim.keymap.set('n', '<leader>ta', '<Cmd>AbortDispatch<CR>')
 
 vim.keymap.set('n', '<leader>tn', '<Cmd>TestNearest<CR>')
@@ -17,3 +16,4 @@ local switchStrategy = function (strategy)
 end
 vim.keymap.set('n', '<leader>tv', switchStrategy('vimux'), { desc = 'Use tmux (vimux) to run tests' })
 vim.keymap.set('n', '<leader>td', switchStrategy('dispatch'), { desc = 'Use :Dispatch to run tests' })
+vim.keymap.set('n', '<leader>tD', switchStrategy('dispatch_background'), { desc = 'Use :Dispatch! to run tests' })
