ebf5dac82774744e464f84168acdaf933ca74a3b
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2024-03-05 21:48:30 +0100

    Finalize migration to new config layout

--- home/.config/nvim/after/plugin-old/floating-preview.vim
+++
@@ -1,3 +0,0 @@
-set completeopt-=preview
-let g:float_preview#docked = 0
-let g:float_preview#max_width = 30
---
+++ home/.config/nvim/lua/plugins/windows.lua
@@ -0,0 +1,19 @@
+return {
+  'anuvyklack/windows.nvim',
+  dependencies = {
+    'anuvyklack/middleclass',
+    'anuvyklack/animation.nvim',
+  },
+  config = function()
+    vim.o.winwidth = 10
+    vim.o.winminwidth = 10
+    require('windows').setup {
+      ignore = {
+        buftype = { 'quickfix' },
+        filetype = { 'Neotest Summary', 'fugitiveblame' },
+      },
+    }
+
+    vim.keymap.set('n', '<A-Enter>', '<Cmd>WindowsMaximize<CR>')
+  end,
+}
