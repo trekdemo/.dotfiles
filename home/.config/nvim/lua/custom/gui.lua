670bda1af86f5f5afcbf2500f6c62fbca4941fae
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2024-03-05 14:10:03 +0100

    Reformat gui.lua

--- home/.config/nvim/lua/custom/gui.lua
+++ home/.config/nvim/lua/custom/gui.lua
@@ -1,14 +1,14 @@
-vim.o.guifont = "VictorMono Nerd Font:h15"
+vim.o.guifont = 'VictorMono Nerd Font:h15'
 -- vim.o.guifont = "JetBrainsMono Nerd Font:h14"
 
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
   vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_input_use_logo=true
-  vim.g.neovide_input_macos_alt_is_meta=true
-  vim.g.neovide_cursor_animation_length=0.02
+  vim.g.neovide_input_use_logo = true
+  vim.g.neovide_input_macos_alt_is_meta = true
+  vim.g.neovide_cursor_animation_length = 0.02
   vim.g.neovide_cursor_trail_size = 0.25
-  vim.g.neovide_cursor_vfx_mode = ""
+  vim.g.neovide_cursor_vfx_mode = ''
   -- vim.g.novide_cursor_vfx_mode = "pixiedust"
 
   vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
