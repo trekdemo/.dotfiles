ba828fc6a03533ebe3f297278aa3733f4dd8563e
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2024-03-06 08:07:47 +0100

    Update vim-test config

--- home/.config/nvim/lua/plugins/vim-test.lua
+++ home/.config/nvim/lua/plugins/vim-test.lua
@@ -8,12 +8,20 @@ return {
         vim.keymap.set('n', "'r", ':Start bundle exec rails console<CR>')
         vim.keymap.set('n', "'m", ':Start rails-mycli<CR>')
         vim.keymap.set('n', "'g", ':Start tig<CR>')
+        vim.keymap.set('n', "'n", ':Start nnn<CR>')
       end,
     },
   },
   config = function()
-    vim.g['test#ruby#rspec#options'] = '--format progress'
-    vim.g['test#strategy'] = 'dispatch'
+    vim.g['test#ruby#rspec#options'] = {
+      nearest = '--format documentation --backtrace',
+      file = '--format documentation',
+      suite = '--format progress',
+    }
+
+    vim.g['test#ruby#rspec#executable'] = 'bundle exec rspec'
+    -- vim.g['test#strategy'] = 'dispatch'
+    vim.g['test#strategy'] = 'vimux'
 
     vim.keymap.set('n', '<leader>tl', '<Cmd>TestLast<CR>', { desc = '[T]est [L]ast' })
     vim.keymap.set('n', '<leader>ta', '<Cmd>AbortDispatch<CR>', { desc = '[T]est Dispatch [A]bort' })
@@ -28,8 +36,8 @@ return {
         print('Using ' .. strategy .. ' to run tests')
       end
     end
-    vim.keymap.set('n', '<leader>tv', switchStrategy 'vimux', { desc = 'Use tmux (vimux) to run tests' })
-    vim.keymap.set('n', '<leader>td', switchStrategy 'dispatch', { desc = 'Use :Dispatch to run tests' })
-    vim.keymap.set('n', '<leader>tD', switchStrategy 'dispatch_background', { desc = 'Use :Dispatch! to run tests' })
+    vim.keymap.set('n', '<leader>tv', switchStrategy 'vimux', { desc = 'Run [T]ests with [V]imux' })
+    vim.keymap.set('n', '<leader>td', switchStrategy 'dispatch', { desc = 'Run [T]ests with [D]ispatch' })
+    vim.keymap.set('n', '<leader>tD', switchStrategy 'dispatch_background', { desc = 'Run [T]ests with [D]ispatch!' })
   end,
 }
