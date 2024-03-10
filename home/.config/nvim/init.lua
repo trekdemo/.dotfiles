a6aa0dbf593f4e0c1ad13a17651d7d3c04a13ed8
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2024-03-10 15:46:53 +0100

    Extract colorscheme into a module

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -41,6 +41,7 @@ require('lazy').setup {
       vim.cmd.cabbrev('mkdir', 'Mkdir!')
     end,
   },
+  require 'plugins.colorscheme',
   require 'plugins.vim-fugitive',
   require 'plugins.which-key',
   require 'plugins.telescope',
@@ -93,25 +94,6 @@ require('lazy').setup {
         -- javascript = { { "prettierd", "prettier" } },
       },
     },
-  },
-
-  -- 'marko-cerovac/material.nvim',
-
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    'folke/tokyonight.nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- Load the colorscheme here
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
-    end,
   },
 
   -- Highlight todo, notes, etc in comments
