d528e2aecbf68c2301c69e2a9900a55a219662b2
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2024-04-25 14:06:16 +0200

    Add configuration for trouble.nvim

---
+++ home/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,17 @@
+return {
+  'folke/trouble.nvim',
+  -- branch = 'dev', -- IMPORTANT! https://github.com/folke/trouble.nvim/tree/dev
+  keys = {
+    {
+      '<leader>xx',
+      '<cmd>TroubleToggle document_diagnostics<cr>',
+      desc = 'Diagnostics (Trouble)',
+    },
+    {
+      '<leader>xX',
+      '<cmd>TroubleToggle workspace_diagnostics<cr>',
+      desc = 'Buffer Diagnostics (Trouble)',
+    },
+  },
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+}
