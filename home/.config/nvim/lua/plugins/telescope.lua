fa6adf8bd08730cc376190ff5f65bc89864d307c
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2024-05-05 14:20:13 +0200

    Add mapping to delete buffers

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -61,7 +61,15 @@ return { -- Fuzzy Finder (files, lsp, etc)
       --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
       --   },
       -- },
-      -- pickers = {}
+      pickers = {
+        buffers = {
+          mappings = {
+            i = {
+              ['<C-d>'] = 'delete_buffer',
+            },
+          },
+        },
+      },
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
