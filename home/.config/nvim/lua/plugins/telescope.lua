4cc628a56210f746900b22574c4793bf5c3553b8
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2024-07-03 13:19:32 +0200

    Stop Telescope with Esc

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -57,11 +57,13 @@ return { -- Fuzzy Finder (files, lsp, etc)
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
       --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
+      defaults = {
+        mappings = {
+          i = {
+            ['<Esc>'] = 'close',
+          },
+        },
+      },
       pickers = {
         buffers = {
           mappings = {
