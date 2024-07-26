9f18e53dcee2f58ea35adc687c0d190c9144a040
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2024-07-26 15:41:44 +0200

    Add erb-lint and erb-formatter

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -105,6 +105,7 @@ require('lazy').setup {
       formatters_by_ft = {
         lua = { 'stylua' },
         markdown = { 'mdformat' }, -- pip3 install mdformat-gfm mdformat-frontmatter mdformat-wikilink
+        eruby = { 'erb-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
