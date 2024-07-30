6296bd01d1394a93678d46c81b94f5509e335448
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2024-07-30 10:05:57 +0200

    Force container architecture for act
    Otherwise it won't run on Apple Silicon Macs

---
+++ home/.config/zsh/conf.d/act.zsh
@@ -0,0 +1,4 @@
+# WARN  ⚠ You are using Apple M-series chip and you have not specified container
+# architecture, you might encounter issues while running act. If so, try running
+# it with '--container-architecture linux/amd64'
+alias act="act --container-architecture linux/amd64"
