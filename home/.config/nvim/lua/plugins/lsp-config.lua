7a1fa9ff45f7d420e7b213669c319ab8c1e9bab1
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2024-10-04 21:40:41 +0200

    Replace ltex with harper
    As grammar checker.

--- home/.config/nvim/lua/plugins/lsp-config.lua
+++ home/.config/nvim/lua/plugins/lsp-config.lua
@@ -163,18 +163,7 @@ return { -- LSP Configuration & Plugins
       yamlls = {},
       tailwindcss = {},
       marksman = {},
-      ltex = {
-        settings = {
-          ltex = {
-            enabled = { 'gitcommit', 'markdown', 'restructuredtext', 'html', 'xhtml', 'mail', 'plaintext' },
-            language = 'en-GB',
-            additionalRules = {
-              -- Downloaded from https://languagetool.org/download/ngram-data/
-              languageModel = '~/.ngrams/',
-            },
-          },
-        },
-      },
+      ['harper-ls'] = { userDictPath = '~/dict.txt' },
       -- pyright = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
