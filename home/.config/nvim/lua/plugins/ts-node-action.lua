6547603d71c045be7ed812ae84c8e1bdfdfe7044
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2024-12-29 11:55:50 +0100

    Add initial markdown ts-node-action actions

--- home/.config/nvim/lua/plugins/ts-node-action.lua
+++ home/.config/nvim/lua/plugins/ts-node-action.lua
@@ -1,26 +1,34 @@
+-- TODO: Insert the link reference after the current paragraph
+-- TODO: Ensure that the reference id is unique in the document
+local function convert_inline_link_to_reference(node)
+  local helpers = require 'ts-node-action.helpers'
+  local components = helpers.destructure_node(node)
+  local id = 1
+
+  return {
+    '[' .. components.link_text .. '][' .. id .. ']',
+    '[' .. id .. ']: ' .. components.link_destination,
+  }
+end
+
+-- TODO: Grab the actual link_destination for the reference
+local function convert_reference_to_inline_link(node)
+  local helpers = require 'ts-node-action.helpers'
+  local components = helpers.destructure_node(node)
+  local link_destination = components.link_label:gsub('[%[%]]', { ['['] = '', [']'] = '' })
+
+  return '[' .. components.link_text .. '](' .. link_destination .. ')'
+end
+
 return {
   'CKolkey/ts-node-action',
-  config = function()
-    local swap_quotes = function(node)
-      local helpers = require 'ts-node-action.helpers'
-      local text = helpers.node_text(node)
-      local wrapped = string.gsub(text, [[(["'])]], function(s)
-        if s == "'" then
-          return '"'
-        elseif s == '"' then
-          return "'"
-        end
-      end)
-
-      return { wrapped }
-    end
-
-    require('ts-node-action').setup {
-      ruby = {
-        ['string'] = swap_quotes,
-      },
-    }
-
-    vim.keymap.set('n', 'T', require('ts-node-action').node_action, { desc = 'Trigger Node Action' })
-  end,
+  keys = {
+    { 'T', '<cmd>NodeAction<CR>', desc = 'Trigger node action' },
+  },
+  opts = {
+    markdown_inline = {
+      ['inline_link'] = convert_inline_link_to_reference,
+      ['full_reference_link'] = convert_reference_to_inline_link,
+    },
+  },
 }
