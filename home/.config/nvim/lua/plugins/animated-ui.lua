3914aa75280eb0309fefd77ad56e0a3b989b3284
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-01-02 19:17:27 +0100

    Remove noice.nvim

--- home/.config/nvim/lua/plugins/animated-ui.lua
+++ home/.config/nvim/lua/plugins/animated-ui.lua
@@ -28,39 +28,4 @@ return {
       }
     end,
   },
-
-  -- Noicer UI
-  {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    opts = {
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-        },
-        -- Lsp Progress is formatted using the builtins for lsp_progress. See
-        -- config.format.builtin See the section on formatting for more details
-        -- on how to customize.
-        progress = { enabled = true },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
-      },
-    },
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      'MunifTanjim/nui.nvim',
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      -- 'rcarriga/nvim-notify',
-    },
-  },
 }
