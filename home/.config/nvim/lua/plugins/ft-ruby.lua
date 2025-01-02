4559e65266625e7fecb586744d453a1a1a63b74a
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-01-02 17:44:50 +0100

    Remove unused vim-blobkle plugin
    I have the ts-node-actions plugin for this.

--- home/.config/nvim/lua/plugins/ft-ruby.lua
+++ home/.config/nvim/lua/plugins/ft-ruby.lua
@@ -1,9 +1,8 @@
 return {
-  { 'jgdavey/vim-blockle', ft = { 'ruby' } },
+  { 'mikepjb/vim-chruby', ft = { 'ruby' } },
   { 'noprompt/vim-yardoc', ft = { 'ruby' } },
   { 'tpope/vim-bundler', ft = { 'ruby' } },
   { 'tpope/vim-rails', ft = { 'ruby' } },
   { 'rbong/vim-flog', ft = { 'ruby' } },
-  { 'mikepjb/vim-chruby', ft = { 'ruby' } },
   { 'tpope/vim-ragtag', ft = { 'eruby', 'html', 'xml', 'javascript.jsx', 'jsx', 'typescript.tsx', 'tsx' } },
 }
