7d15124661cdbf29906ecc59dd385afd8a951ab9
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-01-15 13:46:36 +0100

    Use Alt to navigate vim and tmux panes

--- home/.config/nvim/lua/plugins/nvim-tmux-navigation.lua
+++ home/.config/nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -8,25 +8,25 @@ return {
 
     local map = vim.keymap.set
     local options = { noremap = true, silent = true }
-    map('n', '<C-h>', nav.NvimTmuxNavigateLeft, options)
-    map('n', '<C-j>', nav.NvimTmuxNavigateDown, options)
-    map('n', '<C-k>', nav.NvimTmuxNavigateUp, options)
-    map('n', '<C-l>', nav.NvimTmuxNavigateRight, options)
+    map('n', '<M-h>', nav.NvimTmuxNavigateLeft, options)
+    map('n', '<M-j>', nav.NvimTmuxNavigateDown, options)
+    map('n', '<M-k>', nav.NvimTmuxNavigateUp, options)
+    map('n', '<M-l>', nav.NvimTmuxNavigateRight, options)
 
-    map('n', '<C-Left>', nav.NvimTmuxNavigateLeft, options)
-    map('n', '<C-Down>', nav.NvimTmuxNavigateDown, options)
-    map('n', '<C-Up>', nav.NvimTmuxNavigateUp, options)
-    map('n', '<C-Right>', nav.NvimTmuxNavigateRight, options)
+    map('n', '<M-Left>', nav.NvimTmuxNavigateLeft, options)
+    map('n', '<M-Down>', nav.NvimTmuxNavigateDown, options)
+    map('n', '<M-Up>', nav.NvimTmuxNavigateUp, options)
+    map('n', '<M-Right>', nav.NvimTmuxNavigateRight, options)
     -- vim.api.nvim_set_keymap('n', "<C-\\>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLastActive()<cr>", { noremap = true, silent = true })
     -- vim.api.nvim_set_keymap('n', "<C-Space>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateNext()<cr>", { noremap = true, silent = true })
-    map('t', '<C-h>', '<C-\\><C-n><C-h>', { silent = true })
-    map('t', '<C-j>', '<C-\\><C-n><C-j>', { silent = true })
-    map('t', '<C-k>', '<C-\\><C-n><C-k>', { silent = true })
-    map('t', '<C-l>', '<C-\\><C-n><C-l>', { silent = true })
+    map('t', '<M-h>', '<C-\\><C-n><M-h>', { silent = true })
+    map('t', '<M-j>', '<C-\\><C-n><M-j>', { silent = true })
+    map('t', '<M-k>', '<C-\\><C-n><M-k>', { silent = true })
+    map('t', '<M-l>', '<C-\\><C-n><M-l>', { silent = true })
 
-    map('t', '<C-Left>', '<C-\\><C-n><C-h>', { silent = true })
-    map('t', '<C-Down>', '<C-\\><C-n><C-j>', { silent = true })
-    map('t', '<C-Up>', '<C-\\><C-n><C-k>', { silent = true })
-    map('t', '<C-Right>', '<C-\\><C-n><C-l>', { silent = true })
+    map('t', '<M-Left>', '<C-\\><C-n><M-h>', { silent = true })
+    map('t', '<M-Down>', '<C-\\><C-n><M-j>', { silent = true })
+    map('t', '<M-Up>', '<C-\\><C-n><M-k>', { silent = true })
+    map('t', '<M-Right>', '<C-\\><C-n><M-l>', { silent = true })
   end,
 }
