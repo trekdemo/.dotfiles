06cc8be294552a35dc2b9856f9382c5889169ff6
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-03-15 21:12:07 +0100

    Change how completion works
    Instead of always showing a menu, I use the ghost_text, and I made the
    C-space keyboard shortcut available to display the completion menu when
    needed.

--- home/.config/nvim/lua/plugins/completion.lua
+++ home/.config/nvim/lua/plugins/completion.lua
@@ -1,7 +1,54 @@
 return {
   'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = 'rafamadriz/friendly-snippets',
+  dependencies = {
+    {
+      'zbirenbaum/copilot.lua',
+      config = function()
+        require('copilot').setup {
+          suggestion = { enabled = false },
+          panel = { enabled = false },
+        }
+      end,
+    },
+    'rafamadriz/friendly-snippets',
+    'giuxtaposition/blink-cmp-copilot',
+  },
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = {
+      ['<C-f>'] = { 'select_and_accept', 'fallback' },
+    },
+    completion = {
+      -- Show documentation when selecting a completion item
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      menu = {
+        auto_show = false,
+        draw = {
+          columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 }, { 'kind' } },
+        },
+      },
+      ghost_text = { enabled = true },
+    },
+
+    sources = {
+      -- Remove 'buffer' if you don't want text completions, by default it's only enabled when LSP returns no items
+      default = { 'lsp', 'snippets', 'path', 'copilot', 'buffer' },
+      providers = {
+        snippets = { score_offset = 999 }, -- Show snippets before anything else
+        copilot = {
+          name = 'copilot',
+          module = 'blink-cmp-copilot',
+          score_offset = 100,
+          async = true,
+        },
+      },
+    },
+
+    -- Experimental signature help support
+    signature = { enabled = true },
+  },
 
   -- use a release tag to download pre-built binaries
   version = '*',
@@ -9,39 +56,4 @@ return {
   -- build = 'cargo build --release',
   -- If you use nix, you can build from source using latest nightly rust with:
   -- build = 'nix run .#build-plugin',
-
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = {
-      preset = 'super-tab',
-      ['<C-f>'] = { 'accept', 'fallback' },
-    },
-
-    appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- Useful for when your theme doesn't support blink.cmp
-      -- Will be removed in a future release
-      use_nvim_cmp_as_default = true,
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono',
-    },
-
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
-    sources = {
-      default = { 'snippets', 'lsp', 'buffer', 'path' },
-    },
-    completion = {
-      -- Show documentation when selecting a completion item
-      documentation = { auto_show = true, auto_show_delay_ms = 500 },
-    },
-    signature = { enabled = false },
-  },
-  opts_extend = { 'sources.default' },
 }
