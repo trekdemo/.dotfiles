339b56ac9726d7fdf6d5478d91bef256e786c618
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-04-03 08:54:12 +0200

    Add fzf completion for brew

--- home/.config/zsh/conf.d/fzf.zsh
+++ home/.config/zsh/conf.d/fzf.zsh
@@ -2,3 +2,9 @@
 
 # https://github.com/junegunn/fzf?tab=readme-ov-file#fuzzy-completion-for-bash-and-zsh
 source <(fzf --zsh)
+
+# Add FZF completion for brew commands
+# Type brew <command> **<Tab>
+_fzf_complete_brew() {
+  _fzf_complete --multi --reverse --prompt="brew packages> " -- "$@" < <(brew list)
+}
