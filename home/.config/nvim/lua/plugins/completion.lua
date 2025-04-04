653542539a3280b69885fe06c46c63025dc583ef
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-04-04 10:00:23 +0200

    Turn off avante completion as well until I get Copilot access again

--- home/.config/nvim/lua/plugins/completion.lua
+++ home/.config/nvim/lua/plugins/completion.lua
@@ -43,7 +43,8 @@ return {
     fuzzy = { sorts = { 'exact', 'score', 'sort_text' } },
 
     sources = {
-      default = { 'buffer', 'snippets', 'copilot', 'lsp', 'path', 'avante' },
+      -- default = { 'buffer', 'snippets', 'copilot', 'lsp', 'path', 'avante' },
+      default = { 'buffer', 'snippets', 'lsp', 'path' },
       per_filetype = { sql = { 'dadbod' } },
       providers = {
         snippets = { max_items = 3, min_keyword_length = 1 },
