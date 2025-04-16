35f9b30fcdf86f52bcfc24a51cfbf6ed2882cd03
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-04-16 20:30:47 +0200

    Add aider.nvim to work with aider

--- home/.config/nvim/lua/plugins/ai.lua
+++ home/.config/nvim/lua/plugins/ai.lua
@@ -19,6 +19,16 @@ return {
   --   },
   -- },
   {
+    'joshuavial/aider.nvim',
+    opts = {
+      -- your configuration comes here
+      -- if you don't want to use the default settings
+      auto_manage_context = true, -- automatically manage buffer context
+      default_bindings = true, -- use default <leader>A keybindings
+      debug = false, -- enable debug logging
+    },
+  },
+  {
     'olimorris/codecompanion.nvim',
     dependencies = {
       'nvim-lua/plenary.nvim',
