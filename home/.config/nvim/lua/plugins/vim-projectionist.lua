624409f61f5e28f3a65fd27571da9d289a35cbd9
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-04-22 09:10:22 +0200

    Try out other.nvim

--- home/.config/nvim/lua/plugins/vim-projectionist.lua
+++ home/.config/nvim/lua/plugins/vim-projectionist.lua
@@ -1,12 +1,36 @@
 return {
-  'tpope/vim-projectionist',
-  lazy = false,
-  priority = 99,
-  config = function()
-    -- Global settings, can be overwritten/extended in .projections.json files
-    -- See :help projectionist
-    vim.g.projectionist_heuristics = {
-      ['*.md'] = { type = 'doc' },
-    }
-  end,
+  {
+    'tpope/vim-projectionist',
+    lazy = false,
+    priority = 99,
+    config = function()
+      -- Global settings, can be overwritten/extended in .projections.json files
+      -- See :help projectionist
+      vim.g.projectionist_heuristics = {
+        ['*.md'] = { type = 'doc' },
+      }
+    end,
+  },
+  {
+    'rgroli/other.nvim',
+    main = 'other-nvim',
+    keys = {
+      { '<leader>ll', '<cmd>Other<cr>', desc = 'Alternate files' },
+    },
+    opts = {
+      mappings = {
+        -- builtin mappings
+        'rails',
+        'golang',
+        'python',
+        'react',
+        -- Custom mappings
+        {
+          pattern = '/path/to/file/src/app/(.*)/.*.ext$',
+          target = '/path/to/file/src/view/%1/',
+          transformer = 'lowercase',
+        },
+      },
+    },
+  },
 }
