76348463552c2c5e917fb44e91745ea41cb894c7
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-06-10 10:19:46 +0200

    Add Context7 MCP server

--- home/.config/nvim/lua/plugins/ai.lua
+++ home/.config/nvim/lua/plugins/ai.lua
@@ -48,8 +48,6 @@ return {
     'ravitemer/mcphub.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     build = 'npm install -g mcp-hub@latest', -- Installs `mcp-hub` node binary globally
-    config = function()
-      require('mcphub').setup()
-    end,
+    opts = {},
   },
 }
