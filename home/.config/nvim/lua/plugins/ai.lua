795bf90bc8c5d6fd270efbf58722cdacde4c8e4f
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-08-03 21:36:44 +0200

    Add claude code integration

--- home/.config/nvim/lua/plugins/ai.lua
+++ home/.config/nvim/lua/plugins/ai.lua
@@ -57,10 +57,35 @@ return {
       vim.cmd [[cab cc CodeCompanion]]
     end,
     keys = {
-      { '<leader>at', '<cmd>CodeCompanionChat Toggle<cr>', desc = 'Toggle CodeCompanion' },
-      { '<leader>av', '<cmd>CodeCompanionChat Add<cr>', mode = 'v' },
-      { '<leader>aa', '<cmd>CodeCompanionActions<cr>', mode = { 'n', 'v' } },
+      -- { '<leader>at', '<cmd>CodeCompanionChat Toggle<cr>', desc = 'Toggle CodeCompanion' },
+      -- { '<leader>av', '<cmd>CodeCompanionChat Add<cr>', mode = 'v' },
+      -- { '<leader>aa', '<cmd>CodeCompanionActions<cr>', mode = { 'n', 'v' } },
     },
     cmd = { 'CodeCompanion', 'CodeCompanionChat', 'CodeCompanionCmd', 'CodeCompanionActions' },
+  },
+  {
+    'coder/claudecode.nvim',
+    -- dependencies = { 'folke/snacks.nvim' },
+    provider = 'native', -- "auto", "snacks", "native", or custom provider table
+    config = true,
+    keys = {
+      { '<leader>a', nil, desc = 'AI/Claude Code' },
+      { '<leader>ac', '<cmd>ClaudeCode<cr>', desc = 'Toggle Claude' },
+      { '<leader>af', '<cmd>ClaudeCodeFocus<cr>', desc = 'Focus Claude' },
+      { '<leader>ar', '<cmd>ClaudeCode --resume<cr>', desc = 'Resume Claude' },
+      { '<leader>aC', '<cmd>ClaudeCode --continue<cr>', desc = 'Continue Claude' },
+      { '<leader>am', '<cmd>ClaudeCodeSelectModel<cr>', desc = 'Select Claude model' },
+      { '<leader>ab', '<cmd>ClaudeCodeAdd %<cr>', desc = 'Add current buffer' },
+      { '<leader>as', '<cmd>ClaudeCodeSend<cr>', mode = 'v', desc = 'Send to Claude' },
+      {
+        '<leader>as',
+        '<cmd>ClaudeCodeTreeAdd<cr>',
+        desc = 'Add file',
+        ft = { 'NvimTree', 'neo-tree', 'oil' },
+      },
+      -- Diff management
+      { '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>', desc = 'Accept diff' },
+      { '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', desc = 'Deny diff' },
+    },
   },
 }
