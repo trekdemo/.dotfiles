dacea5dc5a223b62b5d5d7eb83a509a6d2fabbc1
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-08-23 16:51:23 +0200

    dispatch: Remove tmux related settings

--- home/.config/nvim/lua/plugins/vim-dispatch.lua
+++ home/.config/nvim/lua/plugins/vim-dispatch.lua
@@ -1,12 +1,10 @@
 return {
   'tpope/vim-dispatch',
   config = function()
-    vim.keymap.set('n', '<leader>cO', ':Copen!<CR>') -- Load the unfiltered output from the last Dispatch
+    vim.g.dispatch_compilers = { ['bundle exec'] = '', ['bin/'] = '' }
 
-    -- This needs to be set to 1, to be able to use REPL-based debugging
-    -- Also, when the output is messy use :Copen! command to load the
-    -- unformatted test output
-    vim.g['dispatch_tmux_pipe_pane'] = 0
+    vim.keymap.set('n', '<leader>co', ':Copen<CR>') -- Load the output from the last Dispatch
+    vim.keymap.set('n', '<leader>cO', ':Copen!<CR>') -- Load the unfiltered output from the last Dispatch
 
     local map_start_cmd = function(mapping, command)
       vim.keymap.set('n', mapping, ':Start ' .. command .. '<CR>')
