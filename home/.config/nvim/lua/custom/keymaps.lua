b144502ed1da9b46b71b8ceebb653e3d211d8159
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-08-31 19:20:54 +0200

    Remove unused mappings

--- home/.config/nvim/lua/custom/keymaps.lua
+++ home/.config/nvim/lua/custom/keymaps.lua
@@ -11,30 +11,15 @@ vim.keymap.set('n', 'gp', '`[v`]')
 -- Alt + Backspace should delete the last word
 vim.keymap.set({ 'i', 'c', 't' }, '<M-BS>', '<C-w>')
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
--- Save the current buffer if modifiable
-vim.keymap.set('n', '<C-s>', function()
-  vim.cmd.write()
-end, { desc = 'Save file' })
-
 -- Delete every buffer except the current one
 vim.cmd 'command! BufOnly silent! execute "%bd|e#|bd#"'
 vim.keymap.set('n', '<leader>bo', ':BufOnly<CR>')
 
 -- Delete buffer
-vim.keymap.set('n', '<C-x>', ':bd<CR>')
+vim.keymap.set('n', '<C-x>', ':bdelete<CR>')
 
 -- Don't move on *
 vim.keymap.set('n', '*', '*Nzzzv')
--- Center current line on <C-l>
--- vim.keymap.set('n', '<C-l>', '<C-l>zz10<C-e>')
 -- Don't move on J - line join
 vim.keymap.set('n', 'J', 'mzJ`z')
 
@@ -51,8 +36,6 @@ vim.keymap.set({ 'n', 'v', 'o' }, '<End>', '$')
 -- Have relative jumps in the jump list (bigger than 5)
 vim.keymap.set('n', 'j', [[(v:count > 5 ? "m'" . v:count : "") . 'gj']], { expr = true, silent = true })
 vim.keymap.set('n', 'k', [[(v:count > 5 ? "m'" . v:count : "") . 'gk']], { expr = true, silent = true })
-vim.keymap.set('n', '<Up>', 'gk', { silent = true })
-vim.keymap.set('n', '<Down>', 'gj', { silent = true })
 
 -- Undo breakpoints
 vim.keymap.set('i', ',', ',<c-g>u')
@@ -81,7 +64,6 @@ vim.keymap.set('n', ']e', ':move .+1<CR>==', { desc = 'Move line down' })
 
 -- Tab openning and closing
 vim.keymap.set('n', '<leader>tc', ':tabclose<CR>')
-vim.keymap.set('n', '<leader>tt', ':tabnew<CR>:Telescope find_files<CR>')
 
 -- Shorcut for quick substitution
 vim.keymap.set('n', '<leader>ss', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gc<left><left><left>]], { desc = 'Replace in file' })
