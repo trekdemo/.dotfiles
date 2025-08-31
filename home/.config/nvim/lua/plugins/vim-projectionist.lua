03c267643046f69be4574e1d8ea56b5888452651
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-08-31 21:29:26 +0200

    Delete other.nvim

--- home/.config/nvim/lua/plugins/vim-projectionist.lua
+++ home/.config/nvim/lua/plugins/vim-projectionist.lua
@@ -11,26 +11,4 @@ return {
       }
     end,
   },
-  {
-    'rgroli/other.nvim',
-    main = 'other-nvim',
-    keys = {
-      { '<leader>ll', '<cmd>Other<cr>', desc = 'Alternate files' },
-    },
-    opts = {
-      mappings = {
-        -- builtin mappings
-        'rails',
-        'golang',
-        'python',
-        'react',
-        -- Custom mappings
-        {
-          pattern = '/path/to/file/src/app/(.*)/.*.ext$',
-          target = '/path/to/file/src/view/%1/',
-          transformer = 'lowercase',
-        },
-      },
-    },
-  },
 }
