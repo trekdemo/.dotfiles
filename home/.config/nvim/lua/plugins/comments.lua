592a29f96a5485cbc2c574a0256e72fe7383d0fd
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-08-31 20:36:43 +0200

    Remove neogen
    I'm more often use LLMs to generate documentation boilerplate

--- home/.config/nvim/lua/plugins/comments.lua
+++ home/.config/nvim/lua/plugins/comments.lua
@@ -8,16 +8,4 @@ return {
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
-  -- Create annotations with one keybind, and jump your cursor in the inserted
-  -- annotationCreate annotations with one keybind, and jump your cursor in the
-  -- inserted annotation
-  {
-    'danymat/neogen',
-    config = function()
-      local neogen = require 'neogen'
-      neogen.setup { snippet_engine = 'nvim' }
-      vim.keymap.set('n', 'gcg', neogen.generate, { desc = 'Generate documentation' })
-    end,
-  },
 }
