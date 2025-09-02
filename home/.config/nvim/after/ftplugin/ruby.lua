fc5e385c9f52d5026773993dc9115030fb72d261
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-09-02 20:03:42 +0200

    Update my Neovim Ruby setup

--- home/.config/nvim/after/ftplugin/ruby.lua
+++ home/.config/nvim/after/ftplugin/ruby.lua
@@ -1,17 +1,19 @@
 -- Telescope keymaps are in lua/plugins/telescope.lua
+if vim.b.did_ftplugin then
+  return
+end
+vim.b.did_ftplugin = 1
 
 vim.opt_local.iskeyword:append { '?', '!' }
 vim.g.ruby_spellcheck_strings = 1
 
 -- Gary Bernhardt's hashrocket
-vim.keymap.set('i', '<C-l>', '<Space>=><Space>', { desc = 'Insert =>' })
+vim.keymap.set('i', '<C-l>', '<Space>=><Space>', { buffer = true, desc = 'Insert =>' })
 vim.keymap.set('n', '{', '2{w', { buffer = true, desc = 'Jump to previous block' })
 vim.keymap.set('n', '}', '}w', { buffer = true, desc = 'Jump to next block' })
 
 -- See :help b:dispatch
-local file = vim.fn.expand '%'
+local file = vim.fn.expand '%:t'
 if file:match '_spec.rb$' then
-  vim.b['dispatch'] = 'bin/rspec %'
-else
-  vim.b['dispatch'] = 'bundle exec ruby %'
+  vim.b.dispatch = 'bin/rspec %:.'
 end
--- home/.config/nvim/lua/plugins/ft-ruby.lua
+++ home/.config/nvim/lua/plugins/ft-ruby.lua
@@ -1,32 +1,14 @@
-local function configVimRails()
-  -- vim.g.rails_projections = {
-  --   ['packs/*/app/controllers/**_controller.rb'] = {
-  --     type = 'controller',
-  --     alternate = {
-  --       'packs/{dirname}/app/helpers/{basename}_helper.rb',
-  --       'packs/{dirname}/spec/controllers/{basename}_controller_spec.rb',
-  --       'packs/{dirname}/test/controllers/{basename}_controller_test.rb',
-  --     },
-  --     related = {
-  --       'packs/{dirname}/app/models/{singular}.rb',
-  --       'packs/{dirname}/app/views/{basename}/*',
-  --     },
-  --     template = {
-  --       'class {camelcase|capitalize|colons}Controller < ApplicationController',
-  --       'end',
-  --     },
-  --   },
-  -- }
+local function config_vim_rails()
   vim.keymap.set('n', '<leader>ri', ':Eschema<space>', { desc = 'Edit Rails schema' })
+  vim.keymap.set('n', '<leader>rI', ':Vschema<space>', { desc = 'Edit Rails schema' })
 end
 
 return {
-  { 'mikepjb/vim-chruby', ft = { 'ruby' } },
   { 'noprompt/vim-yardoc', ft = { 'ruby' } },
   { 'tpope/vim-bundler', ft = { 'ruby' } },
-  { 'vim-ruby/vim-ruby', ft = { 'ruby' } },
+  { 'vim-ruby/vim-ruby', ft = { 'ruby' }, lazy = false },
   -- See the Rails-related Telescope finders in telescope.lua
-  { 'tpope/vim-rails', ft = { 'ruby' }, config = configVimRails },
+  { 'tpope/vim-rails', ft = { 'ruby' }, config = config_vim_rails, lazy = false },
   { 'rbong/vim-flog', ft = { 'ruby' } },
   { 'tpope/vim-ragtag', ft = { 'eruby', 'html', 'xml', 'javascript.jsx', 'jsx', 'typescript.tsx', 'tsx' } },
 }
