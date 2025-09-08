e29d48c74b3aa1d8e387e69426b82830b1397333
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-09-09 00:02:28 +0200

    Update Ruby LSP settings

--- home/.config/nvim/lsp/ruby_lsp.lua
+++ home/.config/nvim/lsp/ruby_lsp.lua
@@ -1,14 +1,19 @@
--- Install solargraph & rusy-lsp manually for each Ruby version
--- cmd = { 'chruby-exec', '$RUBY_VERSION', '--', 'solargraph', 'stdio' },
-
 return {
+  -- https://shopify.github.io/ruby-lsp/#vs-code-features
   init_options = {
+    featuresConfiguration = {
+      inlayHint = { enableAll = true },
+    },
     indexing = {
       includedPatterns = { 'packs/*/**' },
     },
     addonSettings = {
       ['Ruby LSP Rails'] = {
         enablePendingMigrationsPrompt = false,
+      },
+      -- https://github.com/st0012/ruby-lsp-rspec?tab=readme-ov-file#vs-code-configuration
+      ['Ruby LSP RSpec'] = {
+        debug = false,
       },
     },
   },
--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -1,61 +1,57 @@
 return { -- LSP Configuration & Plugins
   'neovim/nvim-lspconfig',
   dependencies = {
-    { 'kosayoda/nvim-lightbulb', event = 'LspAttach', opts = { autocmd = { enabled = true } } },
+    { 'kosayoda/nvim-lightbulb', opts = { autocmd = { enabled = true } } },
     { 'j-hui/fidget.nvim', opts = {} },
   },
-  config = function()
+  opts = {
+    capabilities = {
+      workspace = {
+        fileOperations = {
+          didRename = true,
+          willRename = true,
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    vim.lsp.config('*', opts)
+
     vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-      callback = function(event)
-        -- NOTE: Remember that lua is a real programming language, and as such it is possible
-        -- to define small helper and utility functions so you don't have to repeat yourself
-        -- many times.
-        --
-        -- In this case, we create a function that lets us more easily define mappings specific
-        -- for LSP related items. It sets the mode, buffer and description for us each time.
-        local map = function(keys, func, desc)
-          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-        end
+      group = vim.api.nvim_create_augroup('user-lsp-attach', { clear = true }),
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
 
-        -- Jump to the implementation of the word under your cursor.
-        --  Useful when your language has ways of declaring types without an actual implementation.
-        -- map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-        -- Rename the variable under your cursor
-        --  Most Language Servers support renaming across files, etc.
-        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-
-        -- Execute a code action, usually your cursor needs to be on top of an error
-        -- or a suggestion from your LSP for this to activate.
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-        -- The following two autocommands are used to highlight references of the
-        -- word under your cursor when your cursor rests there for a little while.
-        --    See `:help CursorHold` for information about when this is executed
-        --
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
         if client and client.server_capabilities.documentHighlightProvider then
           vim.api.nvim_create_autocmd({ 'CursorHold' }, {
-            buffer = event.buf,
+            buffer = args.buf,
             callback = vim.lsp.buf.document_highlight,
           })
 
           vim.api.nvim_create_autocmd({ 'CursorMoved' }, {
-            buffer = event.buf,
+            buffer = args.buf,
             callback = vim.lsp.buf.clear_references,
           })
         end
 
+        -- Enable inlay hints
         if client and client.server_capabilities.inlayHintProvider then
           vim.lsp.inlay_hint.enable(true)
         end
+
+        -- Enable code lenses
+        if client and client.server_capabilities.codeLensProvider then
+          vim.lsp.codelens.refresh { bufnr = args.buf }
+          vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'InsertLeave' }, {
+            buffer = args.buf,
+            callback = vim.lsp.codelens.refresh,
+          })
+        end
+        vim.keymap.set({ 'n', 'v' }, 'grl', vim.lsp.codelens.run, { desc = 'vim.lsp.codelens.run()' })
       end,
     })
 
-    -- Enable the following language servers
     vim.lsp.enable 'gopls'
     vim.lsp.enable 'dockerls'
     vim.lsp.enable 'html'
@@ -67,7 +63,9 @@ return { -- LSP Configuration & Plugins
     vim.lsp.enable 'codebook'
     vim.lsp.enable 'bashls'
     vim.lsp.enable 'lua_ls'
-    vim.lsp.enable 'ruby_lsp' -- gem install ruby-lsp
+    -- Do this to debug https://lsp-devtools.readthedocs.io/en/latest/lsp-devtools/guide/getting-started.html
+    -- vim.lsp.config('ruby_lsp', { cmd = { 'lsp-devtools', 'agent', '--', 'ruby-lsp' } })
+    vim.lsp.enable 'ruby_lsp' -- gem install ruby-lsp ruby-lsp-rails ruby-lsp-rspec
     vim.lsp.enable 'herb_ls' -- npm install -g @herb-tools/language-server
   end,
 }
