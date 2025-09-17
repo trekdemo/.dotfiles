b6bc01fe0e7ad73f21ddf53dbc47334aea811682
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-09-17 21:16:16 +0200

    Use fancy statusline in wezterm

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -5,21 +5,73 @@ config.font_size = 14
 config.line_height = 1.1
 -- config.fond = wezterm.font("VictorMono Nerd Font Mono")
 
-config.color_scheme = "catppuccin-frappe"
+config.color_scheme = "Catppuccin Frappe"
 config.window_decorations = "RESIZE"
 
 config.enable_tab_bar = true
 -- config.tab_bar_at_bottom = true
 
-wezterm.on("update-right-status", function(window, pane)
-	-- "Wed Mar 3 08:14"
-	local date = wezterm.strftime("%b %-d %a")
-	local time = wezterm.strftime("%H:%M")
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+tabline.setup({
+	options = {
+		theme = "Catppuccin Frappe",
+		section_separators = "",
+		component_separators = "",
+		tab_separators = "",
+	},
+	sections = {
+		tabline_a = { "mode" },
+		tabline_b = {},
+		tabline_c = { "domain", "workspace" },
+		tab_active = {
+			"index",
+			-- { "parent", padding = 0 },
+			-- "/",
+			-- { "cwd", padding = { left = 0, right = 1 } },
+			{
+				"process",
+				process_to_icon = {
+					["air"] = { wezterm.nerdfonts.md_language_go },
+					["bacon"] = { wezterm.nerdfonts.dev_rust },
+					["bat"] = { wezterm.nerdfonts.md_bat },
+					["btm"] = { wezterm.nerdfonts.md_chart_donut_variant },
+					["btop"] = { wezterm.nerdfonts.md_chart_areaspline },
+					["bun"] = { wezterm.nerdfonts.md_hamburger },
+					["cargo"] = { wezterm.nerdfonts.dev_rust },
+					["cmd.exe"] = { wezterm.nerdfonts.md_console_line },
+					["curl"] = wezterm.nerdfonts.md_flattr,
+					["debug"] = { wezterm.nerdfonts.cod_debug },
+					["default"] = wezterm.nerdfonts.md_application,
+					["docker"] = { wezterm.nerdfonts.md_docker },
+					["docker-compose"] = { wezterm.nerdfonts.md_docker },
+					["git"] = { wezterm.nerdfonts.dev_git },
+					["kubectl"] = { wezterm.nerdfonts.md_docker },
+					["kuberlr"] = { wezterm.nerdfonts.md_docker },
+					["lazygit"] = { wezterm.nerdfonts.cod_github },
+					["node"] = { wezterm.nerdfonts.md_nodejs },
+					["npm"] = { wezterm.nerdfonts.md_npm },
+					["nvim"] = { wezterm.nerdfonts.custom_neovim },
+				},
+			},
+			{ "zoomed", padding = 0 },
+		},
+		tab_inactive = { "index", { "process", padding = { left = 0, right = 1 } } },
+		tabline_x = { "cpu", "battery" },
+		tabline_y = { "datetime" },
+		tabline_z = {},
+	},
+})
+tabline.apply_to_config(config)
 
-	window:set_right_status(wezterm.format({
-		{ Text = wezterm.nerdfonts.cod_calendar .. " " .. date .. " " .. wezterm.nerdfonts.fa_clock_o .. " " .. time },
-	}))
-end)
+-- wezterm.on("update-right-status", function(window, pane)
+-- 	-- "Wed Mar 3 08:14"
+-- 	local date = wezterm.strftime("%b %-d %a")
+-- 	local time = wezterm.strftime("%H:%M")
+--
+-- 	window:set_right_status(wezterm.format({
+-- 		{ Text = wezterm.nerdfonts.cod_calendar .. " " .. date .. " " .. wezterm.nerdfonts.fa_clock_o .. " " .. time },
+-- 	}))
+-- end)
 
 config.skip_close_confirmation_for_processes_named = { "bash", "sh", "zsh", "tmux" }
 
