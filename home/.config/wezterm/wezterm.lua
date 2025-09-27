2db78d4b95c9ef1e694c92e608b14b195d4fc088
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2025-09-27 21:52:03 +0200

    Shrink wezterm config keymaps

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -5,77 +5,42 @@ local act = wezterm.action
 ---@type Config
 local config = wezterm.config_builder()
 
+config.color_scheme = "Catppuccin Frappe"
+config.bold_brightens_ansi_colors = true
+
+-- Font configuration
 config.font = wezterm.font("VictorMono Nerd Font Mono", { weight = "Medium" })
 config.font_size = 14
 config.line_height = 1.0
-config.bold_brightens_ansi_colors = true
-
-config.color_scheme = "Catppuccin Frappe"
 
 config.window_decorations = "RESIZE"
-config.native_macos_fullscreen_mode = true
 -- config.window_padding = { top = "0.5cell", right = "1cell", bottom = 0, left = "1cell" }
-config.inactive_pane_hsb = { saturation = 0.9, brightness = 0.8 }
 config.window_background_opacity = 0.9
-config.macos_window_background_blur = 20
+config.macos_window_background_blur = 30
+config.native_macos_fullscreen_mode = true
+config.inactive_pane_hsb = { saturation = 0.9, brightness = 0.8 }
 
 config.skip_close_confirmation_for_processes_named = { "bash", "sh", "zsh", "tmux" }
 
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
 	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
-	{
-		key = "a",
-		mods = "LEADER|CTRL",
-		action = act.SendKey({ key = "a", mods = "CTRL" }),
-	},
-	{
-		key = "e",
-		mods = "LEADER",
-		action = act.PromptInputLine({
-			description = wezterm.format({
-				{ Attribute = { Intensity = "Bold" } },
-				{ Foreground = { AnsiColor = "Fuchsia" } },
-				{ Text = "Renaming Tab Title...:" },
-			}),
-			action = wezterm.action_callback(function(window, pane, line)
-				if line then
-					window:active_tab():set_title(line)
-				end
-			end),
-		}),
-	},
-	{
-		key = "w",
-		mods = "CMD",
-		action = act.CloseCurrentPane({ confirm = true }),
-	},
+	{ mods = "LEADER|CTRL", key = "a", action = act.SendKey({ key = "a", mods = "CTRL" }) },
 
-	-- Key table for moving tabs around
-	{ key = "m", mods = "LEADER", action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }) },
+	-- Manage splits
+	{ mods = "CMD", key = "w", action = act.CloseCurrentPane({ confirm = true }) },
+	{ mods = "CMD", key = "d", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ mods = "CMD|SHIFT", key = "d", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 
-	-- Create splits
+	-- Edit settings using nvim
 	{
-		key = "d",
 		mods = "CMD",
-		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "d",
-		mods = "CMD|SHIFT",
-		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
 		key = ",",
-		mods = "CMD",
-		action = wezterm.action.SpawnCommandInNewTab({
+		action = act.SpawnCommandInNewTab({
+			args = { "nvim", os.getenv("WEZTERM_CONFIG_FILE") },
 			cwd = os.getenv("WEZTERM_CONFIG_DIR"),
 			set_environment_variables = {
 				PATH = os.getenv("PATH") .. ":/opt/homebrew/bin/",
-			},
-			args = {
-				"vim",
-				os.getenv("WEZTERM_CONFIG_FILE"),
 			},
 		}),
 	},
